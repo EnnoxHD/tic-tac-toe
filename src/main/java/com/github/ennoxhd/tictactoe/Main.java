12d2f7447a3f0453691a7b431064aa346158be91
EnnoxHD <henrik.schirmacher@outlook.de>
2021-05-13 19:32:32 +0200

    Refactor input and output in Print

--- src/main/java/com/github/ennoxhd/tictactoe/Main.java
+++ src/main/java/com/github/ennoxhd/tictactoe/Main.java
@@ -1,28 +1,24 @@
 package com.github.ennoxhd.tictactoe;
 
-import java.util.Scanner;
-
 public class Main {
 
 	public static void main(String[] args) {
-		try(final Scanner input = new Scanner(System.in)) {
-			Print print = new Print(input);
-			Game game = print.preGame();
-			Turn nextTurn = null;
-			while(!game.getBoard().hasWon()) {
-				print.print(game.getBoard().toString());
-				boolean invalidTurn = false;
-				do {
-					if(invalidTurn) {
-						print.invalidTurn(nextTurn);
-					} else {
-						invalidTurn = true;
-					}
-					nextTurn = print.getTurnFromInput(game.getCurrentPlayer());
-				} while(!game.getBoard().nextTurnValid(nextTurn));
-				game.nextTurn(nextTurn);
-			}
-			print.postGame(game.getWinner());
+		Print print = new Print(System.in, System.out);
+		Game game = print.preGame();
+		Turn nextTurn = null;
+		while(!game.getBoard().hasWon()) {
+			print.print(game.getBoard().toString());
+			boolean invalidTurn = false;
+			do {
+				if(invalidTurn) {
+					print.invalidTurn(nextTurn);
+				} else {
+					invalidTurn = true;
+				}
+				nextTurn = print.getTurnFromInput(game.getCurrentPlayer());
+			} while(!game.getBoard().nextTurnValid(nextTurn));
+			game.nextTurn(nextTurn);
 		}
+		print.postGame(game.getWinner());
 	}
 }
--- src/main/java/com/github/ennoxhd/tictactoe/Print.java
+++ src/main/java/com/github/ennoxhd/tictactoe/Print.java
@@ -1,5 +1,8 @@
 package com.github.ennoxhd.tictactoe;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Print {
@@ -8,29 +11,31 @@ public class Print {
 	private static final String NO = "no";
 	
 	private Scanner input;
+	private PrintStream output;
 	
-	public Print(Scanner input) {
-		this.input = input;
+	public Print(InputStream in, OutputStream out) {
+		input = new Scanner(in);
+		output = new PrintStream(out);
 	}
 	
 	public Game preGame() {
 		final String options = giveOptions(YES, NO);
-		System.out.print("--- Welcome to TicTacToe ---\n"
+		output.print("--- Welcome to TicTacToe ---\n"
 				+ "Do you want to give the players some names (" + options + ")? ");
 		String a = "";
 		int i = 0;
 		do {
 			if(i != 0) {
-				System.out.print("Wrong input, please write " + options + ": ");
+				output.print("Wrong input, please write " + options + ": ");
 			}
 			a = input.nextLine();
 			i++;
 		} while((!YES.equals(a)) && (!NO.equals(a)));
 		if(YES.equals(a)) {
-			System.out.print("Enter the name for the first player: ");
+			output.print("Enter the name for the first player: ");
 			String firstPlayerName = "";
 			firstPlayerName = input.next();
-			System.out.print("Enter the name for the second player: ");
+			output.print("Enter the name for the second player: ");
 			String secondPlayerName = "";
 			secondPlayerName = input.next();
 			return new Game(firstPlayerName, secondPlayerName);
@@ -49,24 +54,24 @@ public class Print {
 	}
 	
 	public Turn getTurnFromInput(Player currentPlayer) {
-		System.out.println();
+		output.println();
 		int[] turnCoords = new int[2];
 		boolean gotInvalidInput = false;
 		do {
 			if(gotInvalidInput) {
-				System.out.println("Please enter coordinates 0 or 1 or 2 for x and y direction!");
+				output.println("Please enter coordinates 0 or 1 or 2 for x and y direction!");
 			} else {
 				gotInvalidInput = true;
 			}
 			if(!currentPlayer.getPlayerName().equals("")) {
-				System.out.println("Move of " + currentPlayer.getPlayerName()
+				output.println("Move of " + currentPlayer.getPlayerName()
 					+ " (" + currentPlayer.getSignString() + "): ");
 			} else {
-				System.out.println("Move of " + currentPlayer.getSignString() + ": ");
+				output.println("Move of " + currentPlayer.getSignString() + ": ");
 			}
-			System.out.print("x - ");
+			output.print("x - ");
 			turnCoords[0] = input.nextInt();
-			System.out.print("y - ");
+			output.print("y - ");
 			turnCoords[1] = input.nextInt();
 		} while(turnCoords[0] < 0 || turnCoords[0] > 2 || turnCoords[1] < 0 || turnCoords[1] > 2);
 		return new Turn(currentPlayer.getSign(), turnCoords[0], turnCoords[1]);
@@ -79,15 +84,15 @@ public class Print {
 		} else {
 			wStr = winner.getSignString();
 		}
-		System.out.println("\nThe winner of the game is: " + wStr);
-		System.out.println("--- --- --- --- ---");
+		output.println("\nThe winner of the game is: " + wStr);
+		output.println("--- --- --- --- ---");
 	}
 	
 	public void print(String string) {
-		System.out.print(string);
+		output.print(string);
 	}
 
 	public void invalidTurn(Turn nextTurn) {
-		System.out.println("The turn " + nextTurn.toString() + " is not valid!");
+		output.println("The turn " + nextTurn.toString() + " is not valid!");
 	}
 }
